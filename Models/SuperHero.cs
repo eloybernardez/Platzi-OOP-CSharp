b4e0fdc82bb2d91d4bd03f4d6057f39b8732e2eb
Eloy Bernardez <eloy.bernardez@zerobug.com.ar>
2024-02-12 18:20:48 -0300

    Agregar archivos de proyecto.

---
+++ Models/SuperHero.cs
@@ -0,0 +1,29 @@
+﻿namespace Platzi_OOP_CSharp.Models
+{
+    public class SuperHero
+    {
+        private string _Nombre;
+        public int Id = 1;
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+            set
+            {
+                _Nombre = value.Trim();
+            }
+        }
+        public string IdentidadSecreta;
+        public string Ciudad;
+        public List<SuperPower> SuperPowers = new List<SuperPower>();
+
+        public SuperHero(string nombre, string identidadSecreta, string ciudad)
+        {
+            Nombre = nombre;
+            IdentidadSecreta = identidadSecreta;
+            Ciudad = ciudad;
+        }
+    }
+}
---
+++ Models/SuperPower.cs
@@ -0,0 +1,17 @@
+﻿namespace Platzi_OOP_CSharp.Models
+{
+    public class SuperPower
+    {
+        public int Id = 1;
+        public string Name;
+        public string Description;
+        public PowerLevel Level;
+    }
+
+    public enum PowerLevel
+    {
+        Low,
+        Medium,
+        High
+    }
+}
